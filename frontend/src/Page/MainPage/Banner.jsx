88ddff933fc9d7daf4f1f0915ca584a3682eb14c
sungha <seongha.roaster@gmail.com>
2025-12-08 18:32:01 +0900

    main mobile img

--- frontend/src/Page/MainPage/Banner.jsx
+++ frontend/src/Page/MainPage/Banner.jsx
@@ -126,7 +126,7 @@ const Banner = () => {
       <div
         className="
           flex md:hidden
-          w-full min-h-[70vh]
+          w-full min-h-[100vh]
           items-center justify-center
           bg-center bg-no-repeat bg-cover relative
         "
