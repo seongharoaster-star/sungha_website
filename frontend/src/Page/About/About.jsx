0760598908c410cca10a5216c2ee1819e9949d5d
sungha <seongha.roaster@gmail.com>
2025-12-09 13:52:44 +0900

    about

--- frontend/src/Page/About/About.jsx
+++ frontend/src/Page/About/About.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import companyImage from "../../assets/officeinfo.jpg";
-import value1 from "../../assets/value1.png";
-import value2 from "../../assets/value2.png";
-import value3 from "../../assets/value3.png";
+import value1 from "../../assets/value1.jpg";
+import value2 from "../../assets/value2.jpg";
+import value3 from "../../assets/value3.jpg";
 
 const About = () => {
   return (
